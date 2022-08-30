f6b5c8150c33076b9ab6771efb01e000667888a2
Muhammad Shahzaib <imshahzaib101@gmail.com>
2022-08-30 22:04:49 +0500

    check /

--- src/components/products/productRoutes.js
+++ src/components/products/productRoutes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const productController = require("./productController");
 
+router.get("/", (req,res)=>{
+    res.send("Server is Working fine")
+});
 router.get("/getProducts", productController.getProducts);
 router.post("/createProduct", productController.createProduct);
 router.put("/updateProduct/:id", productController.updateProduct);
